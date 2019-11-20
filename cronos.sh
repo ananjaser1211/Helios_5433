8ee092c46e304fff300ccfe86d500629adf1a80c
ananjaser1211 <ananjaser@gmail.com>
2019-11-20 23:40:58 +0400

    cronos: update to V3.3 script
    * Measure boot, zimage and dtb sizes after compilation
    * error check zimage and dtb
    * easier IMAGE_NAME function
    * other misc updates

--- cronos.sh
+++ cronos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Cronos Build Script V3.0
+# Cronos Build Script V3.3
 # For Exynos7870
 # Coded by BlackMesa/AnanJaser1211 @2019
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,7 +36,7 @@ CR_RAMDISK_MAIN=$CR_DIR/Helios/Ramdisk
 CR_VERSION=V3.0
 CR_NAME=Helios_Kernel
 # Thread count
-CR_JOBS=9
+CR_JOBS=$((`nproc`-1))
 # Target android version and platform (7/n/8/o)
 CR_ANDROID=7
 CR_PLATFORM=7.0.0
@@ -46,6 +46,7 @@ CR_ARCH=arm
 CR_DATE=$(date +%Y%m%d)
 # Init build
 export CROSS_COMPILE=$CR_TC
+# General init
 export ANDROID_MAJOR_VERSION=$CR_ANDROID
 export PLATFORM_VERSION=$CR_PLATFORM
 export $CR_ARCH
@@ -86,7 +87,9 @@ if [ "$yn" = "Y" -o "$yn" = "y" ]; then
      rm -r -f $CR_DTB
      rm -rf $CR_DTS/.*.tmp
      rm -rf $CR_DTS/.*.cmd
-     rm -rf $CR_DTS/*.dtb      
+     rm -rf $CR_DTS/*.dtb
+     rm -rf $CR_DIR/.config
+     rm -rf $CR_OUT/*.img    
 else
      echo "Dirty Build"
      rm -r -f $CR_DTB
@@ -95,14 +98,28 @@ else
      rm -rf $CR_DTS/*.dtb          
 fi
 
+BUILD_IMAGE_NAME()
+{
+	CR_IMAGE_NAME=$CR_NAME-$CR_VERSION-$CR_VARIANT-$CR_DATE
+}
+
 BUILD_ZIMAGE()
 {
 	echo "----------------------------------------------"
 	echo " "
 	echo "Building zImage for $CR_VARIANT"
-	export LOCALVERSION=-$CR_NAME-$CR_VERSION-$CR_VARIANT-$CR_DATE
+	export LOCALVERSION=-$CR_IMAGE_NAME
+	echo "Make $CR_CONFG"
 	make  $CR_CONFG
 	make -j$CR_JOBS
+	if [ ! -e $CR_KERNEL ]; then
+	exit 0;
+	echo "Image Failed to Compile"
+	echo " Abort "
+	fi
+	du -k "$CR_KERNEL" | cut -f1 >sizT
+	sizT=$(head -n 1 sizT)
+	rm -rf sizT
 	echo " "
 	echo "----------------------------------------------"
 }
@@ -112,14 +129,18 @@ BUILD_DTB()
 	echo " "
 	echo "Building DTB for $CR_VARIANT"
 	# Dont use the DTS list provided in the build script.
-    # This source compiles dtbs while doing Image
-    #make  $CR_CONFG
+	# This source compiles dtbs while doing Image
+	#make  $CR_CONFG
 	#make $CR_DTSFILES
 	./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $CR_DTS/
-	du -k "./boot.img-dtb" | cut -f1 >sizT
-	sizT=$(head -n 1 sizT)
-	rm -rf sizT
-	echo "Combined DTB Size = $sizT Kb"
+	if [ ! -e $CR_DTB ]; then
+	exit 0;
+	echo "DTB Failed to Compile"
+	echo " Abort "
+	fi
+	du -k "$CR_DTB" | cut -f1 >sizdT
+	sizdT=$(head -n 1 sizdT)
+	rm -rf sizdT
 	rm -rf $CR_DTS/.*.tmp
 	rm -rf $CR_DTS/.*.cmd
 	rm -rf $CR_DTS/*.dtb
@@ -131,29 +152,32 @@ PACK_BOOT_IMG()
 	echo "----------------------------------------------"
 	echo " "
 	echo "Building Boot.img for $CR_VARIANT"
-    # General Ramdisk for all variants
+	# General Ramdisk for all variants
 	cp -rf $CR_RAMDISK_MAIN/* $CR_AIK
-    # Device specific changes
+	# Device specific changes
 	cp -rf $CR_RAMDISK_SUB/* $CR_AIK
-    # To avoid any permission issues
-    echo "Fix Ramdisk Permissions"
-    cd $CR_RAMDISK_MAIN
-    find -type d -exec chmod 755 {} \;
-    find -type f -exec chmod 644 {} \;
-    find -name "*.rc" -exec chmod 750 {} \;
-    find -name "*.sh" -exec chmod 750 {} \;
-    chmod -Rf 750 init sbin
-    # Move Compiled kernel and dtb to A.I.K Folder
+	# To avoid any permission issues
+	echo "Fix Ramdisk Permissions"
+	cd $CR_RAMDISK_MAIN
+	find -type d -exec chmod 755 {} \;
+	find -type f -exec chmod 644 {} \;
+	find -name "*.rc" -exec chmod 750 {} \;
+	find -name "*.sh" -exec chmod 750 {} \;
+	chmod -Rf 750 init sbin
+	# Move Compiled kernel and dtb to A.I.K Folder
 	mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
 	mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
-    # Create boot.img
+	# Create boot.img
 	$CR_AIK/repackimg.sh
-    # Remove red warning at boot
+	# Remove red warning at boot
 	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
-    # Move boot.img to out dir
-	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT.img
-    # Cleanup A.I.K Workspace
-    echo " "
+	# Move boot.img to out dir
+	mv $CR_AIK/image-new.img $CR_OUT/$CR_IMAGE_NAME.img
+	# Cleanup A.I.K Workspace
+	du -k "$CR_OUT/$CR_IMAGE_NAME.img" | cut -f1 >sizkT
+	sizkT=$(head -n 1 sizkT)
+	rm -rf sizkT
+	echo " "
 	$CR_AIK/cleanup.sh
 }
 
@@ -170,14 +194,17 @@ do
             CR_CONFG=$CR_CONFG_N910CH
             CR_DTSFILES=$CR_DTSFILES_N910CH
        	    CR_RAMDISK_SUB=$CR_RAMDISK_N910CH
+       	    BUILD_IMAGE_NAME
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_IMAGE_NAME.img Ready"
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -190,14 +217,17 @@ do
        	    CR_CONFG=$CR_CONFG_N910SLK
             CR_DTSFILES=$CR_DTSFILES_N910SLK
        	    CR_RAMDISK_SUB=$CR_RAMDISK_N910SLK
+       	    BUILD_IMAGE_NAME
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_IMAGE_NAME.img Ready"
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -210,14 +240,17 @@ do
        	    CR_CONFG=$CR_CONFG_N910U
             CR_DTSFILES=$CR_DTSFILES_N910U
        	    CR_RAMDISK_SUB=$CR_RAMDISK_N910U
+       	    BUILD_IMAGE_NAME
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_IMAGE_NAME.img Ready"
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -230,14 +263,17 @@ do
        	    CR_CONFG=$CR_CONFG_N915SLK
             CR_DTSFILES=$CR_DTSFILES_N915SLK
        	    CR_RAMDISK_SUB=$CR_RAMDISK_N915SLK
+       	    BUILD_IMAGE_NAME
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_IMAGE_NAME.img Ready"
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -250,14 +286,17 @@ do
        	    CR_CONFG=$CR_CONFG_N916SLK
             CR_DTSFILES=$CR_DTSFILES_N916SLK
        	    CR_RAMDISK_SUB=$CR_RAMDISK_N916SLK
+       	    BUILD_IMAGE_NAME
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_IMAGE_NAME.img Ready"
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
